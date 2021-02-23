145d22c915ec2cd434faf0faab29df254af7afb0
abhikam395 <--add>
2021-02-23 10:09:27 +0530

    change

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('name', function(req, res, next) {
+  res.send('devesh');
+});
+
 module.exports = router;
